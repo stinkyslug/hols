8236172b50f9bf49cfc231d4ecf209e160e8d411
stinkyslug <107139098+stinkyslug@users.noreply.github.com>
2022-06-11 23:15:54 +0800

    Update API.js

--- src/pages/API.js
+++ src/pages/API.js
@@ -54,76 +54,14 @@ const API = () =>
             <code>2025</code>, <code>2026</code>.
           </li>
         </ul>
-        <${Details}
-          summary="Plus(!!!) check out all the goodies you get for free"
-          className=${detailsStyle}
-          data-event="true"
-          data-label="api-features"
-        >
-          <ul>
-            <li>It’s free (<span aria-hidden="true">✨</span>)</li>
-            <li>
-              <a href="https://twitter.com/pcraig3" target="_blank" rel="noopener"
-                >Dedicated support channel</a
-              >
-            </li>
-            <li>Kind of bilingual (EN & FR)</li>
-            <li>
-              Basically compliant with the${' '}
-              <a
-                href="https://www.canada.ca/en/government/system/digital-government/modern-emerging-technologies/government-canada-standards-apis.html"
-                target="_blank"
-                >Government of Canada Standards on APIs</a
-              >${' '} (<span aria-hidden="true">🤙</span>)
-            </li>
-            <li>
-              <a href="https://github.com/pcraig3/hols" target="_blank" rel="noopener"
-                >Open source</a
-              >${' '}which is cool if you’re a nerd
-            </li>
-            <li>
-              <a
-                href="https://github.com/pcraig3/hols/blob/main/reference/Canada-Holidays-API.v1.yaml"
-                target="_blank"
-                rel="noopener"
-                >Documented with an OpenAPI spec</a
-              >${' '}which is <em>even more</em> cool for <em>even nerdier</em> nerds
-            </li>
-          </ul>
+         
         <//>
-        <p>Definitely use it for your billions of dollars mission-critical system.</p>
-
-        <h2>Documentation</h2>
-        <p>
-          There's an OpenAPI spec at${' '}<a
-            href="https://github.com/pcraig3/hols/blob/main/reference/Canada-Holidays-API.v1.yaml"
-            target="_blank"
-            rel="noopener"
-            ><code>Canada-Holidays-API.v1.yaml</code></a
-          >${' '}and a${' '}<a
-            href="https://app.swaggerhub.com/apis/pcraig3/canada-holidays/"
-            target="_blank"
-            rel="noopener"
-            >SwaggerHub</a
-          >${' '}page where you can test the endpoints.
-        </p>
-        <p>
-          There’s also a ${' '}<a
-            title="API documentation"
-            href="https://github.com/pcraig3/hols/blob/main/API.md"
-            target="_blank"
-            rel="noopener"
-            >basic overview on GitHub</a
-          >
-          ${' '}if you’re a “read-the-first-and-last-chapter” kind of person.
-        </p>
 
         <h2>Citations</h2>
         <p>
-          I’ve collected references to${' '}<a href="/sources">all the sources</a>${' '}I’m using
-          for each region in Canada, so data returned from the API should all be accurate.
+          References to${' '}<a href="/sources">all the sources</a>${' '} this webapp uses
+          for each region in Canada, so data returned from the API should be accurate.
         </p>
-        <p>Mistaeks do happen, however — <a href="/feedback">lemme know</a> if you spot any.<//>
 
         <br />
         <br />
