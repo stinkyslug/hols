0e9aeff1b6378b0a5d7098245a76259e32864079
stinkyslug <107139098+stinkyslug@users.noreply.github.com>
2022-06-11 23:07:46 +0800

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -13,34 +13,12 @@ const About = ({ data: { nextHoliday } }) =>
           <a href="/">${nextHoliday.nameEn}</a> is what we have to look forward to.
         </p>
 
-        <h2>“I want to add holidays to my calendar”</h2>
-        <p>
-          Heck yes you do! And${' '}
-          <a
-            href="/add-holidays-to-calendar"
-            title="you can add Canada’s ${getCurrentHolidayYear()} holidays to your calendar"
-            >you can</a
-          >!
-        </p>
 
-        <h2>“[Something] is wrong”</h2>
-        <p>Leave me some <a href="/feedback">/feedback</a> and I’ll do my best.</p>
+        <h2>“My name is Wei Teck”</h2>
+        <p>I've hosted this webapp on Google Cloud Run and pointed my custom domain at it. Enjoy!</p>
 
         <h2>Me</h2>
-        <p>
-          Hello, my name is${' '}
-          <a
-            class="pcraig3"
-            href="https://pcraig3.ca"
-            title="Paul Craig"
-            target="_blank"
-            rel="noopener"
-            >Paul</a
-          >${' '} and I am paying for this site for some reason.${' '}
-          <a href="https://github.com/pcraig3/hols" target="_blank" rel="noopener"
-            >Code is on GitHub</a
-          >${' '}if you want to “borrow” any of my intellectual property.
-        </p>
+        
       <//>
     <//>
   `
