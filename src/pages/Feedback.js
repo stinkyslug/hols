384785f1ff0e5c58d370740762f3ad82fb42eff2
stinkyslug <107139098+stinkyslug@users.noreply.github.com>
2022-06-13 21:53:24 +0800

    Update Feedback.js

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -9,21 +9,7 @@ const Feedback = () =>
       <${Content}>
         <h1>Feedback</h1>
         <p>
-          If you notice any wrong info or weird layouts, please${' '} get in touch. You’ll be
-          helping lots of people out b/c of how crazy good my${' '}
-          <${Abbr} title="Search Engine Optimization">SEO<//> is.
-        </p>
-        <p>
-          On the other hand, if you <span role="img" aria-label="heart">❤️</span> my site and want
-          to support me on Patreon, we can figure that out as well.
-        </p>
-        <p>
-          Email me pls <span aria-hidden="true">👉</span>${' '}
-          <a
-            class="pcraig3"
-            href="mailto:paul@pcraig3.ca?subject=Something is UP with canada-holidays.ca"
-            >paul@pcraig3.ca</a
-          >
+          If you'd like to provide me with any feedback on my assessment, please drop me an email at weiteck.ong.2016@smu.edu.sg!
         </p>
       <//>
     <//>
